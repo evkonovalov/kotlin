7f23344e3f659fc30c96e01013d32aa5f1ab1eb3
Евгений Коновалов < >
2016-07-05 12:31:03 +0700

    Two new tests

--- brainfuck/src/test/kotlin/VmTest.kt
+++ brainfuck/src/test/kotlin/VmTest.kt
@@ -5,24 +5,31 @@ import java.io.File
 
 class VmTest {
 
-    @Test fun helloworld(){
+    @Test fun helloworld(){ //Writes Hello World!
         var vm = Vm();
         vm.reader = "++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.".toByteArray(Charset.defaultCharset());
         vm.start();
         assertEquals("Hello World!", String(vm.str));
     }
 
-    @Test fun div(){
+    @Test fun div(){ //Divides six on two
         var vm = Vm();
         vm.reader = "+>+++++++>++<[<+>>[<<->>->+>]>[<<+>>[<+>-]]<<<<[<]>>-]<-.".toByteArray(Charset.defaultCharset());
         vm.start();
         assertEquals(3.toByte().toChar(), vm.str[0]);
     }
 
-    @Test fun plus(){
+    @Test fun plus(){ //Sum 11 and 2
         var vm = Vm();
         vm.reader = "+++++++++++>++[<+>-]<.".toByteArray(Charset.defaultCharset());
         vm.start();
         assertEquals(13.toByte().toChar(), vm.str[0]);
+    }
+
+    @Test fun factorial(){ //5!
+        var vm = Vm();
+        vm.reader = "+++++>>>>+<<<<[->+[->+>+<<]>[-<+>]>[->[->+>+<<]>[-<+>]<<]>[-]>>[-<<+>>]<<<<<<]>>>>.".toByteArray(Charset.defaultCharset());
+        vm.start();
+        assertEquals(120.toByte().toChar(), vm.str[0]);
     }
 }
